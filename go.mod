ae79f9856555c872ee03623f8b34c2c8ffa1e5f2
oviekshagya <oviekshgy@gmail.com>
2023-08-25 16:09:26 +0700

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/mas/ms-gadaimas-package
+
+go 1.20
---
+++ mas.go
@@ -0,0 +1,7 @@
+package ms_gadaimas_package
+
+import "fmt"
+
+func Say() string {
+	return fmt.Sprintf("MS-GADAIMAS-PACKAGE SAY")
+}
